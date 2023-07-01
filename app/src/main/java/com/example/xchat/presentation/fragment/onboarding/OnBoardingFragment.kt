c08598f438c25bb1432b03082ca9c1ac8b7ce382
Jesil-OT <toboroweijesil@gmail.com>
2023-07-01 04:26:25 +0100

    added navigation graph, toolbar and bottom navigation

---
+++ app/src/main/java/com/example/xchat/presentation/fragment/onboarding/OnBoardingFragment.kt
@@ -0,0 +1,29 @@
+package com.example.xchat.presentation.fragment.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.example.xchat.R
+import com.example.xchat.databinding.FragmentOnboardingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: OnBoardingViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentOnboardingBinding.bind(view)
+        binding.apply {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/xchat/presentation/utils/ViewExt.kt
@@ -0,0 +1,11 @@
+package com.example.xchat.presentation.utils
+
+import android.view.View
+
+fun View.hideView() {
+    visibility = View.GONE
+}
+
+fun View.showView() {
+    visibility = View.VISIBLE
+}
